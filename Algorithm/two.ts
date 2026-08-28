cec52a1d8d8bd67ceb25a845192b2018a337198e
madeibao <2901429479@qq.com>
2026-08-28 17:32:39 +0800

    add some explanation

--- Algorithm/two.ts
+++ Algorithm/two.ts
@@ -1,5 +1,5 @@
 
-let twoSum= function(nums: Array<number>, target: number) {
+let twoSum = function(nums: Array<number>, target: number): number[] {
     for (let i = 0; i < nums.length; i++) {
         let dif = target - nums[i];
         // j = i + 1 的目的是减少重复计算和避免两个元素下标相同
@@ -8,10 +8,11 @@ let twoSum= function(nums: Array<number>, target: number) {
                 return [i,j];
         }
     }
+    return [];
 };
 
 let nums = [2,7,10,12]
 let target = 9
-const res = twoSum(nums, target);
+let result: number[] = twoSum(nums, target);
 
-console.log(res[0],res[1])
+console.log(result[0], result[1])
